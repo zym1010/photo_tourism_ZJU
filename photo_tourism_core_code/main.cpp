7d5698a4825142214b8d8612b4ed16d8e7c28808
Yimeng Zhang <zym1010@gmail.com>
2013-04-28 15:47:49 +0800

    initial commit

---
+++ photo_tourism_core_code/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  photo_tourism_core_code
+//
+//  Created by Zhang Yimeng on 13-4-28.
+//  Copyright (c) 2013年 Zhang Yimeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
