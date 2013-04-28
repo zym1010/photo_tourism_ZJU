0bda29491edb54e0cb931a9fbe2cac2a49fda878
Zhongwen Xu <zhongwen19901215@gmail.com>
2013-04-28 20:30:06 +0800

    num_processes

--- fea_extractor/extractor.py
+++ fea_extractor/extractor.py
@@ -3,12 +3,13 @@ extractor.py written by Zhongwen Xu
 """
 from multiprocessing import Pool
 import os
+import sys
 
 def extract(img_path):
     pass
 
-def main(dir_name, feature):
-    pool = Pool(processes = 10)
+def main(dir_name, feature, num_processes):
+    pool = Pool(processes = num_processes)
     filepaths = []
     for root, dirnames, fnames in os.walk(dir_name):
         for filename in fnames:
@@ -19,6 +20,6 @@ def main(dir_name, feature):
 
 if __name__ == '__main__':
     try:
-        main(sys.argv[0], sys.argv[1])
+        main(sys.argv[0], sys.argv[1], int(sys.argv[2]))
     except ValueError:
-        print 'Usage: ./extractor.py dir_name feature'
+        print 'Usage: ./extractor.py dir_name feature [num_processes]'
