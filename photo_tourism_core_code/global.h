ee1c45171ac7c4bf9297b69735e9a8ee4541da80
Yimeng Zhang <zym1010@gmail.com>
2013-04-28 19:09:14 +0800

    add code for reading arguments.

---
+++ photo_tourism_core_code/global.h
@@ -0,0 +1,16 @@
+//
+//  global.h
+//  photo_tourism_core_code
+//
+//  Created by Zhang Yimeng on 13-4-28.
+//  Copyright (c) 2013年 Zhang Yimeng. All rights reserved.
+//
+
+#ifndef photo_tourism_core_code_global_h
+#define photo_tourism_core_code_global_h
+
+// this is the flag for outputting additional debug info
+#define PHOTO_TOURISM_DEBUG
+
+
+#endif
--- photo_tourism_core_code/main.cpp
+++ photo_tourism_core_code/main.cpp
@@ -7,10 +7,34 @@
 //
 
 #include <iostream>
+#include "global.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
-    // insert code here...
+    
+    //read arguments begins.
+    if (argc < 2) {
+		cerr << "USAGE: " << argv[0] << " <path_to_images> [downscale factor = 1.0]" << endl;
+		return 0;
+	}
+    
+    double downscale_factor = 1.0;
+    
+    if(argc >= 3){
+		downscale_factor = atof(argv[2]);
+    }
+    
+#ifdef PHOTO_TOURISM_DEBUG
+    cerr << "======DEBUGGING INFO BEGINS======" << endl;
+    cerr << "path of the images: " << argv[1] << endl;
+    cerr << "scale factor: " << downscale_factor << endl;
+    cerr << "======DEBUGGING INFO ENDS======" << endl;
+#endif
+    //read argument ends.
+    
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
